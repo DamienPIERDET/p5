242658472495aaf6ce604620317b5915f2bc6bd9
Damien <dp.pierdet@gmail.com>
2021-02-09 10:04:41 +0100

    AGARIO1

---
+++ AGARIO/player.py
@@ -0,0 +1,16 @@
+class Player(object):
+    def __init__(self):
+            self.size = 10
+            self.vitesse = 20
+            self.forme = pass
+            self.couleur = Vector3()
+            self.position = vector2()
+            self.direction = vector2()
+
+    def manger :
+
+    def mourir :
+
+    def deplacer :
+
+    def afficher :
